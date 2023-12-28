30f9a865741e434aeb1d3f58d97ae77f6c1918de
koral14 <105457134+koral14@users.noreply.github.com>
2023-12-27 19:06:49 -0800

    adjusted css

--- src/pages/deck/Deck.js
+++ src/pages/deck/Deck.js
@@ -58,6 +58,7 @@ function Deck() {
         try {
           const response = await fetch(
             `${process.env.REACT_APP_API_BASE_URL}/api/v1/deck/${id}`,
+            // `http://localhost:8000/api/v1/deck/${id}`,
             {
               method: "GET",
               headers: {
